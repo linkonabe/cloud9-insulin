743cd06ecbc42326f0843d8c211dbbdd29924a6c
Bayowa <fola.bayo@zoho.eu>
2023-03-27 01:25:12 +0100

    Add files via upload

---
+++ listr.py
@@ -0,0 +1,17 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+myFruitList[2] = "orange"
+print(myFruitList)
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
